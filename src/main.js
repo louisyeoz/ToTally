ec64fc4df53a3c2e650d2a6dddb562743b4e3868
ZAXGnight3728 <louisyeo.24@ichat.sp.edu.sg>
2026-05-11 11:30:11 +0800

    Foundation of the app

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './style.css';
+
+console.log('ToTally booted.');
+
+document.querySelector('.bottom-nav__add').addEventListener('click', () => {
+  console.log('Add button tapped — modal coming next step.');
+});
